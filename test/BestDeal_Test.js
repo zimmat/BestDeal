b6ac4b6ab2fc4595615be0e47271c74dfb8b87cc
zimmat <zimkhitha@projectcodex.co>
2016-04-22 11:53:36 +0200

    test for best deal modules

--- test/BestDeal_Test.js
+++ test/BestDeal_Test.js
@@ -11,4 +11,10 @@ assert.deepEqual(pricePerAvo(),[3, 3.5, 3.33, 2.9]);
   it('should take an array as an input and calculate cheapest deal (make sure you use just two decimal points',function(){
     assert.deepEqual(cheapestDeal(),3);
   });
+  it('should take an array as an input and calculate most expensive deal (make sure you use just two decimal points',function(){
+  assert.deepEqual(mostExpensiveDeal(),2);
+  });
+  it('should take an array as an input and calculate average deal across the deals (make sure you use just two decimal points',function(){
+  assert.deepEqual(mostExpensiveDeal(),2);
+  });
 });
