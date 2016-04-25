55fe29015307f81ff65b2c533b2625c9ba4060be
zimmat <zimkhitha@projectcodex.co>
2016-04-25 11:01:27 +0200

    best deal module tests

--- test/BestDeal_Test.js
+++ test/BestDeal_Test.js
@@ -3,18 +3,19 @@ var cheapestDeal = require('../cheapestDeal');
 var mostExpensiveDeal = require('../mostExpensiveDeal');
 var averagePrice = require('../averagePrice');
 var assert = require('assert');
+var Dealprice = [3, 3.5, 3.33, 2.9];
 
-describe('best deal', function() {
-  it('should take an array as an input and calculate the Price Per Avo for each deal (make sure you use just two decimal points', function() {
-assert.deepEqual(pricePerAvo(),[3, 3.5, 3.33, 2.9]);
+describe('Best deal',function(){
+  it('should return price per deal',function(){
+    assert.deepEqual(pricePerAvo(), [3, 3.5, 3.33, 2.9]);
   });
-  it('should take an array as an input and calculate cheapest deal (make sure you use just two decimal points',function(){
-    assert.deepEqual(cheapestDeal(),3);
+  it('should return the cheapest deal',function(){
+    assert.equal(cheapestDeal(),2.9);
   });
-  it('should take an array as an input and calculate most expensive deal (make sure you use just two decimal points',function(){
-  assert.deepEqual(mostExpensiveDeal(),2);
+  it('should return the mostExpensiveDeal deal',function(){
+    assert.equal(mostExpensiveDeal(),3.5);
   });
-  it('should take an array as an input and calculate average deal across the deals (make sure you use just two decimal points',function(){
-  assert.deepEqual(mostExpensiveDeal(),2);
+  it('should return the average price deal',function(){
+    assert.equal(averagePrice(),6.37);
   });
 });
