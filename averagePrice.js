711e73705c90dcc6062aa83616c7b6b0e7a485e9
zimmat <zimkhitha@projectcodex.co>
2016-04-25 11:02:12 +0200

    average price

---
+++ averagePrice.js
@@ -0,0 +1,19 @@
+module.exports = function(quantity,price){
+  var Dealprice = [3, 3.5, 3.33, 2.9];
+  var total = 0;
+  var expensiveDeal = 0;
+  Dealprice.forEach(function(deal){
+    total += deal;
+  });
+  console.log(total);
+  Dealprice.forEach(function(deal){
+    total += deal;
+  });
+  var avg = 0;
+  Dealprice.forEach(function(deal){
+  avg = Number((total / Dealprice.length).toFixed(2));
+  });
+
+  console.log(avg);
+  return avg;
+  }
